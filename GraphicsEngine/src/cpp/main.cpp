86b80992b6281cf068c01fd877ea3358027a9866
KylePearce-SoftwareDeveloper <kylepearcesoftwaredeveloper@gmail.com>
2020-06-29 01:10:57 +0100

    #15 - File Cleanup

--- GraphicsEngine/src/cpp/main.cpp
+++ GraphicsEngine/src/cpp/main.cpp
@@ -1,7 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+#include <stb_image/stb_image.h>
 
 #include <glm/glm/glm.hpp>
 #include <glm/glm/gtc/matrix_transform.hpp>
